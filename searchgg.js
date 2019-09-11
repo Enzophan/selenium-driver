335c5488a41c6416cc63d5f44d9ec869a2651af8
Kent <phannhan.dn@gmail.com>
2019-09-11 16:49:16 +0700

    Update

--- searchgg.js
+++ searchgg.js
@@ -3,17 +3,14 @@ var chrome = require('selenium-webdriver/chrome');
 var path = require('chromedriver').path;
 
 
-async function main() {
-    let driver = await new Builder()
-        .forBrowser('chrome')
-        .build();
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('http://www.google.com/ncr');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver'), 1000);
 
-    await driver.get('http://www.google.com/ncr')
-
-    const element = await driver.findElement(By.name('q'))
-
-    await element.sendKeys('webdriver', Key.RETURN)
-    await driver.wait(until.titleIs('webdriver - Google Search'), 10000)
-    await driver.quit()
-}
-main()
+    } finally {
+        await driver.quit();
+    }
+})();
